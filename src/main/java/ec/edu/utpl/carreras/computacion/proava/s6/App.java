ccf5a151338d503343de3dac0151e2c51d7059e9
Juan Tacuri <tacurijuanito28@gmail.com>
2026-05-13 08:25:53 -0500

    v2

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/s6/App.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/s6/App.java
@@ -1,6 +1,10 @@
 package ec.edu.utpl.carreras.computacion.proava.s6;
 
 import ec.edu.utpl.carreras.computacion.proava.s6.util.URLExpanderTask;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 /**
@@ -9,11 +13,17 @@ import java.util.Optional;
 public class App {
 
     public static void main(String[] args) {
-        URLExpanderTask urlExpanderTask = new URLExpanderTask();
-        Optional<String> expandedUrl = urlExpanderTask.expand("https://t.co/TGqER33gn7");
-        expandedUrl.ifPresentOrElse(
-                System.out::println,
-                () -> System.out.println("No se puede leer la URL")
+        var path = Path.of(
+            "C:/Users/juani/Downloads/urls.csv"
         );
+        try (var lines = Files.lines(path)) {
+            lines
+                    .filter(line -> !line.isBlank()) //Eliminar urls vacias. List<String>
+                    .map(String::trim) //Elmininar espacios en blanco en una url. List<String>
+                    .map(URLExpanderTask::expand) //Transformar la url en su version expandida. List<Optinnal<Striing>>
+                    .filter(opt -> opt.isPresent()) //Eliminar urls que no pudieron expandinrse.
+                    .map(Optional::get)//Obtener la url expandida. List<String>
+                    .forEach(System.out::println);
+        } catch (IOException e) {}
     }
 }
